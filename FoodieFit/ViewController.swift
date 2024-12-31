02b21f65ef8d83282f9de55b621ec3aacffbe0a7
krisawyerr <krisawyerrr@gmail.com>
2024-12-31 11:04:56 -0800

    Initial Commit

---
+++ FoodieFit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodieFit
+//
+//  Created by Kris Sawyerr on 12/31/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
