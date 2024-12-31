f7ff38c7f40aecf6a998ae87802759fad757f428
krisawyerr <krisawyerrr@gmail.com>
2024-12-31 11:15:22 -0800

    setup project layout

--- FoodieFit/Views/TabBarViewController.swift
+++ FoodieFit/Views/TabBarViewController.swift
@@ -8,12 +8,10 @@
 import UIKit
 
 class TabBarViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
+#Preview {
+    TabBarViewController()
+}
