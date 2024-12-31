cf63fe528c267e2c3b8eea9891e696938788a948
krisawyerr <krisawyerrr@gmail.com>
2024-12-31 11:14:14 -0800

    Setup project layout

---
+++ FoodieFit/Views/TabBarViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TabBarViewController.swift
+//  FoodieFit
+//
+//  Created by Kris Sawyerr on 12/31/24.
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
